20093363efbbc694636c92d45c233923d375b050
NikDoe <nikd0e@yandex.ru>
2022-07-14 17:34:36 +0300

    feature(express): configure express for ejs

--- index.js
+++ index.js
@@ -3,35 +3,10 @@ const express = require('express');
 const app = express();
 const port = 9000;
 
-// app.use((request, response) => {
-// 	// response.send('middleware');
-// });
+app.set('view engine', 'ejs');
 
 app.get('/', (request, response) => {
-	response.send('this is home page');
-});
-
-app.get('/r/:animals', (request, response) => {
-	const { animals } = request.params;
-	response.send(`this is ${animals} page`);
-});
-
-app.get('/r/:animals/:photoId', (request, response) => {
-	const { animals, photoId } = request.params;
-	response.send(`this is photo ${photoId} on ${animals} page`);
-});
-
-app.get('/search', (request, response) => {
-	const { q } = request.query;
-	response.send(`search : ${q}`);
-});
-
-app.post('/dogs', (request, response) => {
-	response.send('🦮dogs post request');
-});
-
-app.get('*', (request, response) => {
-	response.send('wrong path👽');
+	response.render('home');
 });
 
 app.listen(port, () => {

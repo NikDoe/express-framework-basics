9edd3bd22d21e61a04aed869141659f4f5083b0f
NikDoe <nikd0e@yandex.ru>
2022-07-14 17:44:00 +0300

    feature(express): settings the views directory

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 9000;
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (request, response) => {
 	response.render('home');

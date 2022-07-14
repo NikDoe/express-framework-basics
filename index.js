9e1b94ff09d7441fb400b1760d08ef1172ec6ba4
NikDoe <nikd0e@yandex.ru>
2022-07-14 19:43:45 +0300

    feature(express): use bootstrap w/ express

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const subredditData = require('./data.json');
 const app = express();
 const port = 9000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 

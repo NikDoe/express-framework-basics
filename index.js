6c4169b8d03ad70ab619070361dd442610fd04e0
NikDoe <nikd0e@yandex.ru>
2022-07-14 14:21:58 +0300

    feature(express): working w/ query string

--- index.js
+++ index.js
@@ -11,14 +11,19 @@ app.get('/', (request, response) => {
 	response.send('this is home page');
 });
 
-app.get('/:animals', (request, response) => {
+app.get('/r/:animals', (request, response) => {
 	const { animals } = request.params;
 	response.send(`this is ${animals} page`);
 });
 
-app.get('/:animals/:photoId', (request, response) => {
+app.get('/r/:animals/:photoId', (request, response) => {
 	const { animals, photoId } = request.params;
 	response.send(`this is photo ${photoId} on ${animals} page`);
+});
+
+app.get('/search', (request, response) => {
+	const { q } = request.query;
+	response.send(`search : ${q}`);
 });
 
 app.post('/dogs', (request, response) => {

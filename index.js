8107b6265cfc3e02ff0f3494eb4d4dbe4b3de156
NikDoe <nikd0e@yandex.ru>
2022-07-14 13:53:14 +0300

    feature(express): express routing basics

--- index.js
+++ index.js
@@ -3,8 +3,28 @@ const express = require('express');
 const app = express();
 const port = 9000;
 
-app.use((request, response) => {
-	response.send('this is response');
+// app.use((request, response) => {
+// 	// response.send('middleware');
+// });
+
+app.get('/', (request, response) => {
+	response.send('home page');
+});
+
+app.get('/cats', (request, response) => {
+	response.send('🐱‍👤cats page');
+});
+
+app.get('/dogs', (request, response) => {
+	response.send('🐶dogs page');
+});
+
+app.post('/dogs', (request, response) => {
+	response.send('🦮dogs post request');
+});
+
+app.get('*', (request, response) => {
+	response.send('wrong path👽');
 });
 
 app.listen(port, () => {
